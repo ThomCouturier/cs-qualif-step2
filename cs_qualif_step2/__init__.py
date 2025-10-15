931945c9e36e1c522d8486339c9f7b1d18251adf
Thomas Couturier <thomas.couturier0330@gmail.com>
2025-10-14 20:46:10 -0400

    initial commit

---
+++ cs_qualif_step2/__init__.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import hashlib
+
+def request():   
+    url = "http://0.0.0.0:8000"
+    data = {}
+    with open('../cs-qualif-step2-bruno.json', 'r') as file:
+        data = json.load(file)
+
+    x = requests.post(url, data)
+
+    if(x.status_code == 200):
+        deviceId = (x.json["deviceId"])
+        if(len(deviceId) == 32):
+            print(1)
+
+if __name__ == "__main__":
+    request()
